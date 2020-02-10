d0d704852e695481f3a462fb4f6a5ffa373a43d1
diego.velez <diego.velez1@udea.edu.co>
2020-02-10 16:35:25 -0500

    Se calcula el área de un cuadrado de lado L

--- app/src/main/java/com/edwinacubillos/gitflowg2/MainActivity.kt
+++ app/src/main/java/com/edwinacubillos/gitflowg2/MainActivity.kt
@@ -2,11 +2,20 @@ package com.edwinacubillos.gitflowg2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var etLado: EditText
+    lateinit var buttonC: Button
+    lateinit var textCalcu: TextView
 //
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+
+}
 }
