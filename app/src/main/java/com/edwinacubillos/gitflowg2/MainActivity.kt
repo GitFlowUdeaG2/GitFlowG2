0492af3dc7a6d8f13f3b7533b909a092bba1a812
DiegoTorres538 <fernando.torres@udea.edu.co>
2020-02-10 17:11:57 -0500

    Area triángulo

--- app/src/main/java/com/edwinacubillos/gitflowg2/MainActivity.kt
+++ app/src/main/java/com/edwinacubillos/gitflowg2/MainActivity.kt
@@ -2,11 +2,25 @@ package com.edwinacubillos.gitflowg2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-//
+    lateinit var base:String
+    lateinit var altura:String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        BT_calculo_area_triangulo.setOnClickListener{
+            calculoarea()
+        }
+    }
+
+    fun calculoarea(){
+        base = ET_base_triangulo.text.toString()
+        altura = ET_altura_triangulo.text.toString()
+        val area = (base.toDouble()*altura.toDouble())/2
+        val textAux = "El área del triángulo es de:\n$area U^2"
+        TV_resultado_area_triangulo.text = textAux
     }
 }
